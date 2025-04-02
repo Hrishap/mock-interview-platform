03538d655111a8da9a524f813be1322e126d6315
Hrishap <hrishap3@gmail.com>
2025-04-02 21:12:38 +0530

    interviewCard addition

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -23,10 +25,21 @@ const page = () => {
  <section className='flex flex-col gap-6 mt-8'>
   <h2>Your Interviews</h2>
 <div className='interviews-section'>
-  <p>You haven't taken interviews yet</p>
+  {dummyInterviews.map((interview)=>(
+    <InterviewCard {...interview} key={interview.id}/>
+  ))}
+
+  {/* <p>You haven't taken interviews yet</p> */}
 </div>
  </section>
- <section></section>
+ <section className='flex flex-col gap-6 mt-8'>
+  <h2>Take Interview</h2>
+  <div className='interviews-section'>
+  {dummyInterviews.map((interview)=>(
+    <InterviewCard {...interview} key={interview.id}/>
+  ))}
+  </div>
+ </section>
   </>
   )
 }
